f1d58fd7f14c800e08f203037f6e8ba742e5e2f4
CooLoongWu <742420210@qq.com>
2017-06-30 17:30:55 +0800

    无意义

---
+++ app/src/main/java/com/cooloongwu/coolview/anim/actions/Animations.java
@@ -0,0 +1,28 @@
+package com.cooloongwu.coolview.anim.actions;
+
+import android.view.View;
+
+/**
+ * Created by CooLoongWu on 2017-6-30 17:17.
+ */
+
+public class Animations {
+
+    public static void toLeftOf(View view) {
+
+    }
+
+    public static void toRightOf(View view) {
+
+    }
+
+    public static void belowOf(View view) {
+
+    }
+
+    public static void aboveOf(View view) {
+
+    }
+
+
+}
